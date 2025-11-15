5abd5406e7ec1208844cc2551989ea95e221932a
Isaac Lima de Andrade <devzac.andrade@gmail.com>
2025-11-15 18:14:33 -0300

    feat: add barbershop item component

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,24 @@ import Header from "./_components/header";
 import SearchInput from "./_components/search-input";
 import Image from "next/image";
 import banner from "../public/banner.png";
+import BookingItem from "./_components/booking-item";
+import { prisma } from "../lib/prisma";
+import BarbershopItem from "./_components/barbershop-item";
 
-export default function Home() {
+const Home = async () => {
+  const recommendedBarbershops = await prisma.barbershop.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
+  const popularBarbershops = await prisma.barbershop.findMany({
+    orderBy: {
+      name: "desc",
+    },
+  });
+
   return (
-    <>
+    <main>
       <Header />
       <div className="space-y-4 px-5">
         <SearchInput />
@@ -15,7 +29,38 @@ export default function Home() {
           sizes="100vw"
           className="h-auto w-full"
         />
+
+        <h2 className="text-foreground text-xs font-semibold uppercase">
+          Agendamentos
+        </h2>
+
+        <BookingItem
+          serviceName="Corte de cabelo"
+          barbershopName="Barberia do joao"
+          barbershopImageUrl="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"
+          date={new Date()}
+        />
+
+        <h2 className="text-foreground text-xs font-semibold uppercase">
+          Recomendadas
+        </h2>
+        <div className="[&::-webkit-scrollbar]:hidden] flex gap-4 overflow-x-auto">
+          {recommendedBarbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+
+        <h2 className="text-foreground text-xs font-semibold uppercase">
+          Populares
+        </h2>
+        <div className="[&::-webkit-scrollbar]:hidden] flex gap-4 overflow-x-auto">
+          {popularBarbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
-    </>
+    </main>
   );
-}
+};
+
+export default Home;
